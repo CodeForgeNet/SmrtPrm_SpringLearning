6b89a75066567b943a9309c01b6c80b2cbb70cdd
CodeForgeNet <codeforgenet@icloud.com>
2025-02-27 12:53:21 +0530

    update modified files

--- SpringProgram4/src/in/sp/main/Main.java
+++ SpringProgram4/src/in/sp/main/Main.java
@@ -4,10 +4,11 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import in.sp.beans.Student;
+import in.sp.resources.SpringConfigFile;
 
 public class Main {
 	public static void main(String[] args) {
-		ApplicationContext context = new AnnotationConfigApplicationContext("SpringConfigFile.class");
+		ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfigFile.class);
 		
 		Student std = (Student) context.getBean("student");
 		std.display();
--- SpringProgram4/src/in/sp/resources/SpringConfigFile.java
+++ SpringProgram4/src/in/sp/resources/SpringConfigFile.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("in.sp.beans")
+//@ComponentScan("in.sp.beans")
+//@ComponentScan({"in.sp.beans","------","----"})  //This is for multiple.
+@ComponentScan(basePackages = {"in.sp.beans"})
 public class SpringConfigFile {
 
 }
