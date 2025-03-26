1f550a2875a0b9c54c34d35ee982559a6ed53792
CodeForgeNet <codeforgenet@icloud.com>
2025-03-26 22:09:30 +0530

    update spring jdbc project

--- SpringJDBCcrudOper/src/main/java/in/sp/main/App.java
+++ SpringJDBCcrudOper/src/main/java/in/sp/main/App.java
@@ -1,9 +1,15 @@
 package in.sp.main;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import in.sp.resources.SpringConfigFile;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfigFile.class);
+		
+		context.getBean(JdbcTemplate.class);
+	}
 }
