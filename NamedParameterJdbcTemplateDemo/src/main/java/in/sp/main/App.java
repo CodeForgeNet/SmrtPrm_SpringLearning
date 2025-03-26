56e4bc9d0cf5fcf4ff6ac6e0c838791aad1a6b20
CodeForgeNet <codeforgenet@icloud.com>
2025-03-27 01:15:46 +0530

    update named parameter jdbc update

--- NamedParameterJdbcTemplateDemo/src/main/java/in/sp/main/App.java
+++ NamedParameterJdbcTemplateDemo/src/main/java/in/sp/main/App.java
@@ -1,13 +1,37 @@
 package in.sp.main;
 
-/**
- * Hello world!
- *
- */
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import in.sp.resources.SpringConfigFile;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfigFile.class);
+        
+        NamedParameterJdbcTemplate npJdbcTemp = context.getBean(NamedParameterJdbcTemplate.class);
+        
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("key_rollno", 104);
+        map.put("key_name", "Radha");
+        map.put("key_marks", 88.5f);
+        
+        String insert_query = "INSERT INTO student VALUES(:key_rollno, :key_name, :key_marks)";
+        
+        int count = npJdbcTemp.update(insert_query,map);
+        
+        if(count>0) {
+        		System.out.println("insertion completed");
+        }else {
+        		System.out.println("insertion failed");        	
+        }
+        
+        
     }
 }
