9b73cc8c918d271fc5e017238078c5b0fd9883f6
CodeForgeNet <codeforgenet@icloud.com>
2025-03-27 00:37:59 +0530

    update named jdbc spring project

--- NamedParameterJdbcTemplateDemo/src/main/java/in/sp/resources/SpringConfigFile.java
+++ NamedParameterJdbcTemplateDemo/src/main/java/in/sp/resources/SpringConfigFile.java
@@ -1,5 +1,31 @@
 package in.sp.resources;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
 public class SpringConfigFile {
 
+	@Bean
+	public DriverManagerDataSource myDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/spring_jdbc_db");
+		dataSource.setUsername("root");
+		dataSource.setPassword("karan123");
+		
+		return dataSource;
+	}
+	
+	
+	@Bean
+	public NamedParameterJdbcTemplate npJdbcParameter() {
+		
+		return new NamedParameterJdbcTemplate(myDataSource());
+		
+	}
+	
 }
