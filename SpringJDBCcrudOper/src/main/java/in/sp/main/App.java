7004ac93f128a332349674b08237aa360ba9d94c
CodeForgeNet <codeforgenet@icloud.com>
2025-03-26 23:37:37 +0530

    update spring jdbc project

--- SpringJDBCcrudOper/src/main/java/in/sp/main/App.java
+++ SpringJDBCcrudOper/src/main/java/in/sp/main/App.java
@@ -9,21 +9,52 @@ import in.sp.resources.SpringConfigFile;
 public class App {
 	public static void main(String[] args) {
 		
-		int std_rollno = 101;
-		String std_name="Karan";
-		float std_marks = 97.5f;
+		
 		
 		ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfigFile.class);
 		
 		JdbcTemplate jdbcTemp = context.getBean(JdbcTemplate.class);
 		
-		String insert_sql_query = "INSERT INTO student values(?,?,?)";
-		int count = jdbcTemp.update(insert_sql_query,std_rollno,std_name,std_marks);
 		
-		if(count>0) {
-			System.out.println("insertion successfull");
-		}else {
-			System.out.println("insertion failed"); 
-		}
+//		INSERT OPERATION
+		
+//		int std_roll = 102;
+//		String std_name="Arjun";
+//		float std_marks = 95.3f;
+		
+//		String insert_sql_query = "INSERT INTO student values(?,?,?)";
+//		int count = jdbcTemp.update(insert_sql_query,std_roll,std_name,std_marks);
+//		
+//		if(count>0) {
+//			System.out.println("insertion successfull");
+//		}else {
+//			System.out.println("insertion failed"); 
+//		}
+		
+		
+		
+		
+//		UPDATE OPERATION 
+		
+//		float marks = 98.2f;
+//		int roll = 102;
+//		
+//		String update_query = "UPDATE student SET std_marks=? where std_roll=?";
+//		int count = jdbcTemp.update(update_query,marks,roll);
+//		
+//		if(count>0) {
+//			System.out.println("updation successfull");
+//		}else {
+//			System.out.println("updation failed");
+//			
+//		}
+		
+		
+		
+//		DELETE OPERATION 
+		
+		
+		
+		
 	}
 }
