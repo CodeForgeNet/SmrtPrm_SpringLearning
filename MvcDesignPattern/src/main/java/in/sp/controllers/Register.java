312159a7ce4b37acc9df205721f78aa7380a0a13
CodeForgeNet <codeforgenet@icloud.com>
2025-03-28 11:50:39 +0530

    add connection to mvc pattern design

--- MvcDesignPattern/src/main/java/in/sp/controllers/Register.java
+++ MvcDesignPattern/src/main/java/in/sp/controllers/Register.java
@@ -1,5 +1,32 @@
 package in.sp.controllers;
 
-public class Register {
+import java.io.IOException;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/regForm")
+public class Register extends HttpServlet {
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String myName = req.getParameter("name1");
+		String myEmail = req.getParameter("email1");
+		String myPass = req.getParameter("pass1");
+		String myCity = req.getParameter("city1");
+		
+		
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
 }
---
+++ MvcDesignPattern/src/main/java/in/sp/dbCon/DbConnection.java
@@ -0,0 +1,15 @@
+package in.sp.dbCon;
+
+public class DbConnection {
+
+	public void getConnection() {
+		try {
+			
+			Class.forName("");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
