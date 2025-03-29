f02799910b1e43d28049cb76fc54e86b7da4dbd9
CodeForgeNet <codeforgenet@icloud.com>
2025-03-29 11:48:04 +0530

    update spring jdbc project

--- MvcDesignPattern/src/main/java/in/sp/dbCon/DbConnection.java
+++ MvcDesignPattern/src/main/java/in/sp/dbCon/DbConnection.java
@@ -1,15 +1,24 @@
 package in.sp.dbCon;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DbConnection {
 
-	public void getConnection() {
+	public Connection getConnection() {
+		
+		Connection con=null;
+		
 		try {
 			
-			Class.forName("");
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mvc_db","root","karan123");
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		return con;
 	}
 	
 }
